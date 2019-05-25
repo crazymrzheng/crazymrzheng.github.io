6f799403c3e09679f9f37b46a028ae0114c6b775
crazymrzheng <chenmuzheng@foxmail.com>
2019-05-25 09:39:34 +0800

    A batch file for starting my applications

---
+++ startmyapps.bat
@@ -0,0 +1,5 @@
+@echo off
+start /d "C:\Program Files\Internet Explorer\" iexplore.exe "https://cn.bing.com/"
+start /d "C:\Program Files\Mozilla Firefox" firefox.exe
+rem start /d "Path" *.exe
+exit
